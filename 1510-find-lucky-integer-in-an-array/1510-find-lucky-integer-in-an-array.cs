43fff6fae20f058ac1fc5d21f0cda278e5f3d33c
BaoQuoc <75488164+Alligator1506@users.noreply.github.com>
2025-03-23 23:28:10 +0700

    Time: 0 ms (100%), Space: 43.1 MB (8.33%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cs
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cs
@@ -1,26 +1,40 @@
 public class Solution {
     public int FindLucky(int[] arr)
     {
-        Dictionary<int, int> frequency = new Dictionary<int, int>();
-        foreach (int num in arr)
+        int[] numbers = new int[501];
+        foreach (int j in arr)
         {
-            if (frequency.ContainsKey(num))
+            numbers[j]++;
+        }
+
+        for (int i = 500; i > 0; i--)
+        {
+            if (i == numbers[i])
             {
-                frequency[num]++;
-            }
-            else
-            {
-                frequency[num] = 1;
+                return i;
             }
         }
-        int luckyNumber = -1;
-        foreach (KeyValuePair<int, int> pair in frequency)
-        {
-            if (pair.Key == pair.Value && pair.Key > luckyNumber)
-            {
-                luckyNumber = pair.Key;
-            }
-        }
-        return luckyNumber;
+        return -1;
+        // Dictionary<int, int> frequency = new Dictionary<int, int>();
+        // foreach (int num in arr)
+        // {
+        //     if (frequency.ContainsKey(num))
+        //     {
+        //         frequency[num]++;
+        //     }
+        //     else
+        //     {
+        //         frequency[num] = 1;
+        //     }
+        // }
+        // int luckyNumber = -1;
+        // foreach (KeyValuePair<int, int> pair in frequency)
+        // {
+        //     if (pair.Key == pair.Value && pair.Key > luckyNumber)
+        //     {
+        //         luckyNumber = pair.Key;
+        //     }
+        // }
+        // return luckyNumber;
     }
 }
