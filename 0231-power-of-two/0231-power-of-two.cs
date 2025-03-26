ab44fd7c9758156380a8a57e692a755c23aaf1c3
BaoQuoc <75488164+Alligator1506@users.noreply.github.com>
2025-03-26 16:53:02 +0700

    Time: 0 ms (100%), Space: 30.9 MB (60%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cs
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -1,8 +1,11 @@
-public class Solution {
-    public bool IsPowerOfTwo(int n) {
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
         if (n <= 0) return false;
 
-        while (n > 1) {
+        while (n > 1)
+        {
             if (n % 2 != 0) return false;
             n /= 2;
         }
